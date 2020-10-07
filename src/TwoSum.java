e338eaacb1d881f7b6d0bf5c55279b855c169883
Sterling <sterlingtyson97@gmail.com>
2020-10-06 20:16:16 -0400

    TwoSum method fixed and completed

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,30 +1,52 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
-// atnoerh test
-//tst3
 
 public class TwoSum {
-    public int getTwoSum (int [] nums){
 
+    /**
+     * getTwoSum method COMPLETED
+        - no test made (void method)
+     * @param nums
+     */
 
-        try {
-            Scanner scanner = new Scanner(System.in);
-            int targetNum = scanner.nextInt();
-            //int tmp = 0;
+    public void getTwoSumTwo(int [] nums){
+        //Ex: [{3,4,1,5,2}]
+        //Test input: ts.getTwoSumTwo(new int [] {1,2,3,4,5});
+        //Test Input 2: ts.getTwoSumTwo(new int [] {3,6,1,13,9});
 
-            for (int i = 1; i < nums.length-1; i++){
-                int tmp = targetNum - nums[i];
-                //tmp++;
-                if(targetNum - nums[i] == tmp){
-                    System.out.println(nums[i] + " " + tmp);
-                }
-
-            }
-        }catch (Exception e){
-            System.out.println("Some'n fcked up");
+        ArrayList al = new ArrayList<Integer>();
+        for (int nbr: nums) {
+            al.add(nbr);
         }
 
-        return 0;
+        al.sort(Comparator.naturalOrder());
+
+        int []newArray = new int[al.size()];
+
+        for(int i = 0; i< al.size(); i++){
+            newArray[i] = (int) al.get(i);
+        }
+
+        try{
+            System.out.println("Enter a number foo");
+            Scanner scanner = new Scanner(System.in);
+            int targetNum = scanner.nextInt();
+
+            for (int i = 0; i < al.size(); i++){
+                int tmp = targetNum - newArray[i];
+
+                if(targetNum - newArray[i] == tmp && tmp > 0 && newArray[i] > 0 && al.contains(tmp)){
+                    System.out.println(newArray[i] + " " + tmp);
+                }
+            }
+
+        } catch (Exception e){
+            System.out.println("Well ill be damned");
+        }
+
     }
-    public TwoSum (){}
-    //test
+
+    //default constructor
+    //public TwoSum (){}
 }
